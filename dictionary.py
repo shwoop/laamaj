c03dd41de61dc38edb93fb2e974304e0eca6555d
Alistair Ferguson <monkey.mambo.al.ferguson@gmail.com>
2012-06-01 19:55:59 +0100

    new file containing lookups to nltk/wordnet

---
+++ dictionary.py
@@ -0,0 +1,17 @@
+#    Seperate module for querying a dictionary
+#    Using NLTK and WordNet
+from nltk.corpus import wordnet
+
+def lookup_dictionary(check_word)
+    # feed in a word and return a long string of definition
+    counter = 1
+    output = ""
+    synsets = wordnet.synsets(check_word)
+    for synset in synsets:
+        output = output + check_word + ": " + str(counter) + "* " + synset.definition + "\n"
+        counter = counter + 1
+    #print(output)
+    return output
+
+#print (lookup_dictionary("space"))
+
