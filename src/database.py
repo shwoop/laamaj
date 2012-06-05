f0f37d4c56db5ae90fbeb3443ede7500521eb1e4
shwoop <shwoop@shwoop.(none)>
2012-06-05 18:45:31 +0100

    corrected faults from last commit in database src

--- src/database.py
+++ src/database.py
@@ -7,6 +7,10 @@ class Database:
     self.__con = None
     self.__cur = None
     self.__db = database
+    self.__connect()
+
+  def __del__(self):
+    self.__close()
 
   def __connect(self):
     try:
@@ -30,62 +34,31 @@ class Database:
     if self.__con:
       self.__con.commit()
 
-  def __run_query(self, sequel):
-    if self.__con and self.__cur:    
-      if isinstance(sequel, str):
-        try:
-          self.__cur.execute(sequel)
-          print("query run")
-          data = self.__cur.fetchall()
-          if data:
-            print("Query Successful")
-            return data;
-        except sqlite3.Error, e:
-          print("Error : " + e.args[0])
-          return False
-      else:
-        print("sequel not a string")
-    else:
-      print("connection and/or cursor not valid")
-
   def add_website(self, user, chan, website):
     print("adding website")
-    self.__connect()
-    output = self.__run_query("INSERT INTO websites (ws_date, ws_user, ws_chan, ws_url) VALUES (date('now'), '?', '?', '?');", user, chan, websites)
-    self.__commit()
-    self.__close()
+    #self.__connect()
+    output = self.__cur.execute("INSERT INTO websites (ws_date, ws_user, ws_chan, ws_url) VALUES (date('now'), ?, ?, ?);", (user, chan, website))
+    #self.__commit()
+    #self.__close()
     return output
 
   def list_last_sites(self, numero=5):
     print("output sites")
     try:
-      self.__connect()
-      data = self.__run_query("select ws_user||\' - \'||ws_chan||\' - \'||ws_url from websites where ws_id > (select max(ws_id) from websites) - ? order by ws_id desc;", str(numero))
+      #self.__connect()
+      print((str(numero)))
+      data = self.__cur.execute("select ws_user||\' - \'||ws_chan||\' - \'||ws_url from websites where ws_id > (select max(ws_id) from websites) - ? order by ws_id desc;", (str(numero)))
       if data:
         print (data)
+      else:
+        data = "No Results"
+      #self.__close()
+      return(data)
     except sqlite3.Error, e:
       print("Error : ",e.args[0])
-      data = False
-    finally:
-      self.__close()
-      return data
+      data = "Error : {error}".format(error=e.args[0])
+      #self.__close()
+      return(data)
     
 
 
-
-
-
-
-if __name__ == "__main__":
-  db = Database()
-  try:
-    db.__connect()
-    data = db.__run_query("select count(*) from websites")
-    if data:
-      for d in data:
-        print(d[0])
-  except sqlite3.Error, e:
-    print("Error : "+e.args[0])
-  finally:
-    db.__close()
-    db = None
