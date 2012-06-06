c15bbb732f4adbcbceb5e7ce9589778b58209aec
Alistair Ferguson <monkey.mambo.al.ferguson@gmail.com>
2012-06-06 21:57:33 +0100

    went from 2 to 4 space indentation

--- src/database.py
+++ src/database.py
@@ -1,69 +1,69 @@
 ##
-##    DATABSE
-##  
-##  TODO
-##  * checking for dropped connection
+##        DATABSE
+##    
+##    TODO
+##    * checking for dropped connection
 ##
 
 import sqlite3
 
 class Database:
-  """
-     Database Class
-     Connects on init and has add_website and list_last_sites for adding or reading a website from the database.
-  """
-  def __init__(self, database="../db/laamaj.db"):
-    self.__con = None
-    self.__cur = None
-    self.__db = database
-    self.__connect()
+    """
+    Database Class
+    Connects on init and has add_website and list_last_sites for adding or reading a website from the database.
+    """
+    def __init__(self, database="../db/laamaj.db"):
+        self.__con = None
+        self.__cur = None
+        self.__db = database
+        self.__connect()
 
-  def __del__(self):
-    self.__close()
+    def __del__(self):
+        self.__close()
 
-  def __connect(self):
-    try:
-      self.__con = sqlite3.connect(self.__db)
-      self.__cur = self.__con.cursor()
-      print("Connected to "+self.__db)
-    except sqlite3.Error, e:
-      print("Error : "+e.args[0])
+    def __connect(self):
+        try:
+            self.__con = sqlite3.connect(self.__db)
+            self.__cur = self.__con.cursor()
+            print("Connected to "+self.__db)
+        except sqlite3.Error, e:
+            print("Error : "+e.args[0])
 
-  def __close(self):
-    if self.__con:
-      self.__con.close()
-      self.__con = None
-      self.__cur = None
-      print("connection closed")
-    else:
-      print("no open connection")
-      self.__cur = None
+    def __close(self):
+        if self.__con:
+            self.__con.close()
+            self.__con = None
+            self.__cur = None
+            print("connection closed")
+        else:
+            print("no open connection")
+            self.__cur = None
 
-  def close(self):
-    self.__close()
+    def close(self):
+        self.__close()
 
-  def __commit(self):
-    if self.__con:
-      self.__con.commit()
+    def __commit(self):
+        if self.__con:
+            self.__con.commit()
 
-  def add_website(self, user, chan, website):
-    print("adding website")
-    output = self.__cur.execute("INSERT INTO websites (ws_date, ws_user, ws_chan, ws_url) VALUES (date('now'), ?, ?, ?);", (user, chan, website))
-    self.__con.commit()
-    return output
+    def add_website(self, user, chan, website):
+        print("adding website")
+        output = self.__cur.execute("INSERT INTO websites (ws_date, ws_user, ws_chan, ws_url) VALUES (date('now'), ?, ?, ?);", (user, chan, website))
+        self.__con.commit()
+        return output
 
-  def list_last_sites(self, numero=5):
-    print("output sites")
-    try:
-      print((str(numero)))
-      data = self.__cur.execute("select ws_user||\' - \'||ws_chan||\' - \'||ws_url from websites where ws_id > (select max(ws_id) from websites) - ? order by ws_id desc;", (str(numero)))
-      if data:
-        print (data)
-      else:
-        data = "No Results"
-      return(data)
-    except sqlite3.Error, e:
-      print("Error : ",e.args[0])
-      data = "Error : {error}".format(error=e.args[0])
-      return(data)
-    
+    def list_last_sites(self, numero=5):
+        print("output sites")
+        try:
+            print((str(numero)))
+            data = self.__cur.execute("select ws_user||\' - \'||ws_chan||\' - \'||ws_url from websites where ws_id > (select max(ws_id) from websites) - ? order by ws_id desc;", (str(numero)))
+            if data:
+                print (data)
+            else:
+                data = "No Results"
+            return(data)
+        except sqlite3.Error, e:
+            print("Error : ",e.args[0])
+            data = "Error : {error}".format(error=e.args[0])
+            return(data)
+        
--- src/message.py
+++ src/message.py
@@ -1,42 +1,42 @@
 ##
-##    message class
-##  Parse recieved IRC message into relevant secions
+##        message class
+##    Parse recieved IRC message into relevant secions
 ##
 
 import re
 
 class Message:
-  handle = ''
-  user = ''
-  locale = ''
-  mtype = ''
-  channel = ''
-  message = ''
-  message_action = ''
-  message_focus = ''
-  def parse_msg(self, raw_message):
-    parsed = re.match(":(\w+)!~(\w+)@(.*) (\w+) (#\w+) :(.*)",raw_message)
-    if parsed:
-      self.handle = parsed.group(1)
-      self.user = parsed.group(2)
-      self.locale = parsed.group(3)
-      self.mtype = parsed.group(4)
-      self.channel = parsed.group(5)
-      self.message = parsed.group(6)
-      parsed = re.match("!(\w+) *(\w*)", self.message)  # analyse message to look for actions !dict etc
-      if parsed:
-        self.message_action = parsed.group(1)
-        if parsed.group(2):  # need to check as there doesn't need to be a focus for the action
-          self.message_focus = parsed.group(2)
-      else:
-        self.message_action = "NO ACTION"
-        self.message_focus = "NO FOCUS"
-      return 1
-    else:
-      return -1
+    handle = ''
+    user = ''
+    locale = ''
+    mtype = ''
+    channel = ''
+    message = ''
+    message_action = ''
+    message_focus = ''
+    def parse_msg(self, raw_message):
+        parsed = re.match(":(\w+)!~(\w+)@(.*) (\w+) (#\w+) :(.*)",raw_message)
+        if parsed:
+            self.handle = parsed.group(1)
+            self.user = parsed.group(2)
+            self.locale = parsed.group(3)
+            self.mtype = parsed.group(4)
+            self.channel = parsed.group(5)
+            self.message = parsed.group(6)
+            parsed = re.match("!(\w+) *(\w*)", self.message)    # analyse message to look for actions !dict etc
+            if parsed:
+                self.message_action = parsed.group(1)
+                if parsed.group(2):    # need to check as there doesn't need to be a focus for the action
+                    self.message_focus = parsed.group(2)
+            else:
+                self.message_action = "NO ACTION"
+                self.message_focus = "NO FOCUS"
+            return 1
+        else:
+            return -1
 
 # Quick Test
 if __name__ == "__main__":
-  m = Message()
-  m.parse_msg(":SHWOOP!~SHWOOP@192.168.0.1 PRIVMSG #PERTHROAD :!dict lead")
-  print (m.message_action+" "+m.message_focus)
+    m = Message()
+    m.parse_msg(":SHWOOP!~SHWOOP@192.168.0.1 PRIVMSG #PERTHROAD :!dict lead")
+    print (m.message_action+" "+m.message_focus)
