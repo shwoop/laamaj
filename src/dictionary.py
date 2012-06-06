4480547cab7b39392f18a37879b26902732b21e6
Alistair Ferguson <monkey.mambo.al.ferguson@gmail.com>
2012-06-06 21:45:13 +0100

    reformatted

--- src/dictionary.py
+++ src/dictionary.py
@@ -3,14 +3,16 @@
 from nltk.corpus import wordnet
 
 def lookup_synset(check_word):
-  # Return the whole synset from wordnet
-  return wordnet.synsets(check_word)
+    # Return the whole synset from wordnet
+    return wordnet.synsets(check_word)
 
 def lookup_definition(check_word):
-  # extract and return list of definitions from wordnet
-  synsets = wordnet.synsets(check_word)
-  definitions = []
-  for synset in synsets:
-    definitions.append(synset.definition)
-  return definitions
+    # extract and return list of definitions from wordnet
+    synsets = wordnet.synsets(check_word)
+    definitions = []
+    
+    for synset in synsets:
+        definitions.append(synset.definition)
+    
+    return definitions
 
