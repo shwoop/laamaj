8ca95f0ba80e454f8803bdb0c8dcc391e09900a8
shwoop <shwoop@shwoop.(none)>
2012-06-06 15:36:51 +0100

    tidied up a bit

--- src/database.py
+++ src/database.py
@@ -1,8 +1,17 @@
-import sqlite3
-#import sys
+##
+##    DATABSE
+##  
+##  TODO
+##  * checking for dropped connection
+##
 
+import sqlite3
 
 class Database:
+  """
+     Database Class
+     Connects on init and has add_website and list_last_sites for adding or reading a website from the database.
+  """
   def __init__(self, database="../db/laamaj.db"):
     self.__con = None
     self.__cur = None
@@ -30,35 +39,31 @@ class Database:
       print("no open connection")
       self.__cur = None
 
+  def close(self):
+    self.__close()
+
   def __commit(self):
     if self.__con:
       self.__con.commit()
 
   def add_website(self, user, chan, website):
     print("adding website")
-    #self.__connect()
     output = self.__cur.execute("INSERT INTO websites (ws_date, ws_user, ws_chan, ws_url) VALUES (date('now'), ?, ?, ?);", (user, chan, website))
-    #self.__commit()
-    #self.__close()
+    self.__con.commit()
     return output
 
   def list_last_sites(self, numero=5):
     print("output sites")
     try:
-      #self.__connect()
       print((str(numero)))
       data = self.__cur.execute("select ws_user||\' - \'||ws_chan||\' - \'||ws_url from websites where ws_id > (select max(ws_id) from websites) - ? order by ws_id desc;", (str(numero)))
       if data:
         print (data)
       else:
         data = "No Results"
-      #self.__close()
       return(data)
     except sqlite3.Error, e:
       print("Error : ",e.args[0])
       data = "Error : {error}".format(error=e.args[0])
-      #self.__close()
       return(data)
     
-
-
