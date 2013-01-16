c92e2e0e5e9ba774912c911f3dda9174f9f8082d
David MacKeddie <dave.mackeddie@gmail.com>
2013-01-16 20:30:21 +0000

    Added ability to reconnect to irc server
    Added a simple state system to Irc.py.  It will now attempt to reconnect
    to a server when it loses connection.  If the nickname it wants is in use,
    it will append a number until the server lets it log in.  It'll even
    reclaim the nick it wants later.

--- src/irc.py
+++ src/irc.py
@@ -19,19 +19,23 @@ class Irc:
         self.onCtcpHandlers = []
         self.onJoinHandlers = []
         self.onPartHandlers = []
+        self.onQuitHandlers = []
         
-        self.connected = False      
-        #self.Connect(server, port, nick, ident, realname)
+        self.connected = False
+
+        self.reconnectWait = 10 
+        self.wantNick = nick
 
     def connect(self):
         self.__connect_to_server(self.server, self.port, self.nick, self.ident, self.realname)
+        self.state = self.stateConnect
 
     def __connect_to_server(self, server, port, nick, ident, realname):
-        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.s.connect((server, port))
-        self.send("NICK %s" % nick)
-        self.send("USER %s %s %s :%s" % (ident, server, nick, realname))
-        self.readbuf = ""
+        self.server = server
+        self.port = port
+        self.nick = nick
+        self.ident = ident
+        self.realname = realname
     
     def add_on_raw_handler(self, obj):
         self.onRawHandlers.append(obj)
@@ -75,6 +79,10 @@ class Irc:
         for obj in self.onPartHandlers:
             obj(self, who, channel)
 
+    def __call_on_quit_handlers(self, who, message):
+        for obj in self.onQuitHandlers:
+            obj(self, who, message)
+
     def __on_raw(self, text):
         if (DEBUG_IRC):
             print(text)
@@ -103,6 +111,11 @@ class Irc:
     def __on_part(self, who, channel):
         self.__call_on_part_handlers(who, channel)
 
+    def __on_quit(self, who, message):
+        if who == self.wantNick:
+            self.send('NICK ' + self.wantNick)
+        self.__call_on_quit_handlers(who, message)
+
     def __is_ctcp_msg(self, msg):
         if (len(msg) > 0):
             if (msg[0] == chr(1) and msg.endswith(chr(1))):
@@ -126,9 +139,55 @@ class Irc:
         if (DEBUG_IRC):
             print('-> ' + text)
         self.s.send(text + '\r\n')
+
+    def stateConnect(self):
+        try:
+            print('Connecting to %s' % self.server)
+            self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.s.connect((self.server, self.port))
+            self.state = self.stateConnecting
+        except socket.error:
+            print('Unable to connect to %s' % self.server)
+            self.s.close()
+            self.state = self.stateReconnect
         
-    def process(self):
-        while 1:
+    def stateConnecting(self):
+        self.send("NICK %s" % self.nick)
+        self.send("USER %s %s %s :%s" % (self.ident, self.server, self.nick, self.realname))
+        self.readbuf = ""
+
+        self.connected = False
+
+        nickTemp = 0;
+
+        try:
+            while not self.connected:
+                self.readbuf = self.readbuf + self.s.recv(2048)
+                temp = string.split(self.readbuf, '\n');
+                self.readbuf = temp.pop();
+
+                for line in temp:
+                    line = string.rstrip(line)
+                    fullline = line
+                    line = string.split(line)
+
+                    if fullline.find("Nickname is already in use") != -1:
+                        self.nick = self.wantNick + str(nickTemp)
+                        nickTemp += 1
+                        self.send("NICK %s" % self.nick)
+                        self.send("USER %s %s %s :%s" % (self.ident, self.server, self.nick, self.realname))
+
+                    if (line[1] == '001'):
+                        self.connected = True
+                        self.__on_connected(self.server)
+                        self.state = self.stateConnected
+        except socket.error:
+            print('Lost connection to %s' % self.server)
+            self.s.close()
+            self.state = self.stateReconnect
+
+    def stateConnected(self):
+        try:
             #this next 3 lines of apparent madness are to
             #ensure we only parse full lines.  We have no
             #guarantee that each chunk of data coming in
@@ -141,14 +200,10 @@ class Irc:
                 line = string.rstrip(line)
                 fullline = line
                 line = string.split(line)
-                
+
                 if (line[0] == 'PING'):
                     self.send('PONG %s' % line[1])
 
-                if (line[1] == '001'):
-                    self.connected = True
-                    self.__on_connected(self.server)
- 
                 self.__on_raw(fullline)
                 
                 msgfrom = line[0].split('!')[0].strip(':')
@@ -167,4 +222,26 @@ class Irc:
                 if (line[1] == 'PART'):
                     channel = line[2]
                     self.__on_part(msgfrom, channel)
+
+                if (line[1] == 'QUIT'):
+                    msg = ' '.join(line[2:])[1:]
+                    self.__on_quit(msgfrom, msg)
+
+        except socket.error:
+            print('Lost connection to %s' % self.server)
+            self.s.close()
+            self.state = self.stateReconnect
+
+    def stateDisconnected(self):
+        print('Disconnected from %s' % self.server)
+        self.state = self.stateReconnect
+
+    def stateReconnect(self):
+        time.sleep(self.reconnectWait)
+        print('Reconnecting to %s' % self.server)
+        self.state = self.stateConnect
+
+    def process(self):
+        while 1:
+            self.state()
 
