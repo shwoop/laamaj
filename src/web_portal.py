18f0407017313dff3200a45bcef26d4c8cab3fb3
shwoop <monkey.mambo.al.ferguson@gmail.com>
2013-07-05 13:10:52 +0000

    Toying with flask.  Nothing running live yet.

--- src/web_portal.py
+++ src/web_portal.py
@@ -5,17 +5,28 @@ Stand along web portal using the laamaj resources.
 """
 
 import database
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 _db = database.Database()
 
 @app.route("/")
-def bummed_in_the_gob():
+def relay_bitg():
     """ Forward users accessing the domain to the youtube video. """
-    return """<meta HTTP-EQUIV="REFRESH" content="0; \
-        url=http://www.youtube.com/watch?v=my2NVhUjekA">"""
+    return render_template("redirect.htm",
+        target="http://www.youtube.com/watch?v=my2NVhUjekA")
+
+
+@app.route("/tor")
+@app.route("/bitg2")
+@app.route("/atlas")
+@app.route("/relay")
+def relay_atlas_bitg2():
+    """ Redirect tor related queries to the atlas for BITG2. """
+    return render_template("redirect.htm",
+        target="https://atlas.torproject.org/#details/50D921F0D34F5D4E74F86EDB90F3E9F10A89DC01")
+
 
 @app.route("/test")
 def test():
@@ -26,6 +37,7 @@ def test():
         output = output +  str(site[0]) + ',<br \>'  
     output = output + 'fin.'
     return output
+
 
 @app.route("/test2")
 def test2():
@@ -40,6 +52,7 @@ def test2():
     output = num_of_websites + '<br />' + num_of_images
     return output
 
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
-    #print test2()
+    #app.run(host='0.0.0.0', port=80, debug=True)
