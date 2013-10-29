4388408f1faf1ba4b9a58ebde82d2178c9274047
shwoop <monkey.mambo.al.ferguson@gmail.com>
2013-10-29 12:56:35 +0000

    +unicode

--- src/config.py
+++ src/config.py
@@ -1,36 +1,36 @@
 #!/usr/bin/python
 # vim: set fileencoding=utf-8 :
-"""
+'''
 Laamaj Config Module.
 Parse config file for configuration and override values with any
 command line arguments.
-"""
+'''
 
 import sys
 import argparse
 
 
 _requirements = [
-    'NICK','CONFIG','CHANNEL',
-    'SERVER','IDENT','REALNAME'
+    u'NICK',u'CONFIG',u'CHANNEL',
+    u'SERVER',u'IDENT',u'REALNAME'
     ]
 
 
 def _parse_config(options):
-    """ Parse the config file. """
+    ''' Parse the config file. '''
 
     try:
-        f = open(options['CONFIG'])
+        f = open(options[u'CONFIG'])
     except:
-        print 'ERROR: Cannot open config {0}'.format(options['CONFIG'])
+        print u'ERROR: Cannot open config {0}'.format(options[u'CONFIG'])
         sys.exit() 
     for line in f:
     
-        if '#' in line:
-            text, comment = line.split('#',1)
+        if u'#' in line:
+            text, comment = line.split(u'#',1)
     
-        if '=' in line:
-            opt, val = line.split('=',1)
+        if u'=' in line:
+            opt, val = line.split(u'=',1)
             opt = opt.strip()
             val = val.strip()
             if opt not in options.keys():
@@ -42,47 +42,47 @@ def _parse_config(options):
 
 
 def _parse_arguments(options):
-    """ Parse runtime arguments. """
+    ''' Parse runtime arguments. '''
 
     aParse = argparse.ArgumentParser(
-        description='Laamaj IRC Bot\nGrabber of links/pictures \
+        description=u'Laamaj IRC Bot\nGrabber of links/pictures \
         and hopefully shower of them'
         )
     aParse.add_argument(
-        '-t', '--test', action='store_true',
-        help='Engage test mode (join EFNET #laamajtest'
+        u'-t', u'--test', action=u'store_true',
+        help=u'Engage test mode (join EFNET #laamajtest'
         )
     aParse.add_argument(
-        '-c', '--config',
-        help='Specify config file (default .../laamaj/config.cfg'
+        u'-c', u'--config',
+        help=u'Specify config file (default .../laamaj/config.cfg'
         )
     args = aParse.parse_args()
 
     if args.test:
         toupdate = {
-            'TESTMODE':True,'NICK':'laamajtest',
-            'IDENT':'laamajtest','CHANNEL':'laamajtest'
+            u'TESTMODE':True,u'NICK':u'laamajtest',
+            u'IDENT':u'laamajtest',u'CHANNEL':u'laamajtest'
             }
         for k, v in toupdate.iteritems():
             options[k] = v
     
     if args.config:
-        options['CONFIG'] = args.config
+        options[u'CONFIG'] = args.config
 
     return options
 
 
 def _option_exists(options, req):
-    """ Check option has been set. """
+    ''' Check option has been set. '''
 
     if req not in options.keys():
-        print "ERROR: {0} has not been set.".format(req)
+        print u'ERROR: {0} has not been set.'.format(req)
         sys.exit()
     return True
 
 
 def _validate_options(options):
-    """ Check all required options have been set in config. """
+    ''' Check all required options have been set in config. '''
 
     for requirement in _requirements:
         _option_exists(options, requirement)
@@ -90,15 +90,15 @@ def _validate_options(options):
 
 
 def get_parameters():
-    """
+    '''
     Return config dictionary accorging to config file overridden by
     command line arguments.
     
     TODO:
     Add list of config keys
-    """
+    '''
     
-    options = {'TESTMODE':False, 'CONFIG':'../config.cfg'}
+    options = {u'TESTMODE':False, u'CONFIG':u'../config.cfg'}
     options = _parse_arguments(options)
     options = _parse_config(options) 
     _validate_options(options)
@@ -106,6 +106,6 @@ def get_parameters():
     return options
 
 
-if __name__ == '__main__':
-    """ test. """
+if __name__ == u'__main__':
+    ''' test. '''
     print get_parameters()
