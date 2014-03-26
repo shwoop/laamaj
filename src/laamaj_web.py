eb1cce7c2551cb758a2395238daaae9dea51c906
shwoop <monkey.mambo.al.ferguson@gmail.com>
2014-03-26 08:47:22 +0100

    Web end now lists pages of content and links to individual hosted content.  Thus far only youtube is supported.

--- src/laamaj_web.py
+++ src/laamaj_web.py
@@ -11,15 +11,23 @@ from flask import Flask, render_template, url_for, session, jsonify, request
 from tornado.wsgi import WSGIContainer
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
+from re import match
 
 app = Flask(__name__)
 app.secret_key = u'herpy derpy doo'
 
-_db = database.Database()
+_db = database.Database(database=u'/home/shwoop/laamaj/db/laamaj.db')
 
 REC_PER_PAGE = 10
 FETCH_RECORDS_SQL = u'select ws_user, ws_url from websites where \
 ws_id between {0} and {1} order by ws_id desc'
+
+FETCH_RECORDS_SQL_2 = u'select ws_id, ws_user, ws_url, ws_date, ws_localfile \
+from websites where ws_id between {0} and {1} order by ws_id desc'
+
+FETCH_SINGLE_RECORD = u'select ws_id, ws_user, ws_url, ws_date, ws_localfile \
+from websites where ws_id = {0}'
+
 
 @app.route(u'/')
 def relay_bitg():
@@ -45,6 +53,61 @@ def ttg():
     results = _db.exe(FETCH_RECORDS_SQL.format(tail, head))
     output = render_template(u'main.htm', urllist=results)
     session[u'lastrecord'] = tail
+    return output
+
+def pad_page_results(results):
+    ''' generate truncated name based on url and convert image name to bool. '''
+    MAXNAME = 60
+    output = []
+    for key, user, url, date, local in results:
+        name = url
+        if len(url) > MAXNAME:
+            name = url.split(u'/')[-1]
+            if name == u'':
+                ## trailing / leaves empty string at end of list
+                name = url.split(u'/')[-2]
+            name = u''+ url[0:MAXNAME-len(name)] + u"\u2026" + name
+        ## if there's a local image, or the link is to youtube
+        ## set local to true so we can host link
+        local = bool(local)
+        if not local:
+            if match(u'^https?://(www.)?youtube.com.*', url):
+                local = True
+        record = [key, user, url, name, date, local]
+        output.append(record)
+    return output
+
+@app.route(u'/page<int:pagenum>')
+def pages(pagenum):
+    ''' mark 2 or laamaj site '''
+    head = int(_db.exe(u'select max(ws_id) from websites')[0][0])
+    head = max(0, head - ((pagenum -1) * REC_PER_PAGE)) 
+    tail = max(0, head - REC_PER_PAGE)
+    results = _db.exe(FETCH_RECORDS_SQL_2.format(tail, head))
+    results = pad_page_results(results)
+    minpage = max(1, pagenum - 2)
+    pagelist = range(minpage, minpage + 5)
+    output = render_template(u'page.htm',
+                page=pagenum, links=results, pages = pagelist)
+    return output
+
+@app.route(u'/hosted/<int:record>')
+def hosted_content(record):
+    head = int(_db.exe(u'select max(ws_id) from websites')[0][0])
+    if record < 1 or record > head:
+        return u'Get Tae Fuck', 404
+    key, user, url, dat, local = _db.exe(FETCH_SINGLE_RECORD.format(record))[0]
+    if local:
+        output = u'image'
+    else:
+        if match(u'^https?://(www.)?youtube.com.*', url):
+            ## Hack the url to embed and force https
+            url = url.replace('watch?v=','embed/')
+            url = url.replace('http:','https:')
+            output = render_template(u'yatube.htm', record=record,
+                        user=user, dat=dat, url=url)
+        else:
+            output = u'away', 404
     return output
 
 @app.route(u'/cert')
